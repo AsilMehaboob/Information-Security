1c491067a40db5b16a9adf2757cb48fc3c4f0da7
AsilMehaboob <asilmehaboob@gmail.com>
2026-03-12 20:21:20 +0530

    Added File List Shell Program

---
+++ Shell Programs/filelist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir="."
+count=1
+
+echo "S.No | File Name | Creation Date"
+echo "--------------------------------"
+
+for file in "$dir"/*
+do 
+	if [ -f "$file" ]
+	then 
+		creation_date=$(stat -c %w "$file")
+		if [ "$creation_date" = "-" ];then
+			creation_date=$(stat -c %y "$file" | cut -d' ' -f1)
+		fi
+
+	echo  "$count | $(basename "$file") | $creation_date"
+	count=$((count+1))
+	fi
+done
